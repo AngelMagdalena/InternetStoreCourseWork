14e926180c53d03f8cae3e7f1fe277dd411f40f5
mrs.Kislinskaya <kiss-linka@ukr.net>
2018-05-28 15:23:34 +0300

    Create interfaces: IMainCategoryManager, IProductManager, ISubCategoryManager, IUserManager, IUserGroupManager

---
+++ BLL/Interfaces/IProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Entities;
+
+namespace BLL.Interfaces
+{
+   public interface IProductManager
+    {
+        void CreateProduct(Product product, User author, SubCategory sub_category);
+        void EditProduct(Product product, SubCategory sub_cat);
+        void RemoveProduct(int id);
+        bool CheckCode(string code);
+        Product GetProduct(int id);
+        List<Product> Filter(IEnumerable<Product> Products, Product Product);
+        List<Product> GetAllProducts();
+        void Dispose();
+    }
+}
